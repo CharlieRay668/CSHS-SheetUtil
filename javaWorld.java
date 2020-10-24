75be0fa86c7e9e278c85495abbea630d6dea296b
ebnewson10 <ebnewson01@bvsd.org>
2020-10-23 21:28:42 -0600

    trying

--- javaWorld.java
+++ javaWorld.java
@@ -1,9 +1,12 @@
+
+
 public class javaWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         System.out.println("Hello World From Java!");
         System.out.println("Hello World From Charlie!");
         System.out.println("Hello World from Jo!");
         System.out.println("Hello World from Lauren!");
         System.out.println("Hello World from Ethan!!!");
+
+        }
     }
-}

650f0c3c83038c785c8784a8616f242108c3d627
CharlieRay668 <charlie.ray320@gmail.com>
2020-10-24 16:16:31 -0600

    Created JavaCode project moved CsvArr into project

--- JavaCode/src/main/CsvToArrayList.java
+++ JavaCode/src/main/CsvToArrayList.java
@@ -1,10 +1,14 @@
+package JavaCode.src.main;
+
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.io.File;
+
 public class CsvToArrayList {
     public static void main(String[] args) throws FileNotFoundException {
+        //System.out.println("Hello");
         Scanner reader = new Scanner(new File("fake_data.csv"));
         List<ArrayList<Object>> TwoDArrayList = new ArrayList<ArrayList<Object>>();
         while(reader.next() != ",");

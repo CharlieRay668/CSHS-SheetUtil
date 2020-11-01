3a9b5625d32dfff08dd04215c5b1c1fd629a0362
ebnewson10 <ebnewson01@bvsd.org>
2020-10-31 23:10:04 -0700

    CSVTOMap

---
+++ JavaCSVToArrayList/CsvTest.java
@@ -0,0 +1,16 @@
+package JavaCSVToArrayList;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class CsvTest {
+    public static void main(String[] args) throws FileNotFoundException {
+        CsvToMap tester = new CsvToMap(new File("fake_data.csv"));
+        Map<String,ArrayList<String>> test = tester.csvToMap();
+        System.out.println(test);
+        System.out.println("Student:" + test.get("name").get(1) + " Attendance:" + test.get("2020/10/22-08:50").get(1));
+        System.out.println(tester.getStudentsAttendance(test,"2020/10/22-08:50","Charlie Ray"));
+    }
+}
---
+++ JavaCSVToArrayList/CsvToMap.java
@@ -0,0 +1,71 @@
+package JavaCSVToArrayList;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CsvToMap {
+    private File csv;
+    public CsvToMap(File f){
+        csv = f;
+    }
+    public Map<String,ArrayList<String>> csvToMap() throws FileNotFoundException {
+        Scanner reader = new Scanner(csv);
+        Map<String, ArrayList<String>> map = new HashMap<String,ArrayList<String>>();
+        ArrayList<String> key = new ArrayList<String>();
+        key.add("name");
+        int lvl = 0;
+        while(reader.hasNext()){
+            String str =reader.nextLine();
+            String vtr = str;
+
+            if(str.startsWith(",")){
+                vtr = str.substring(1);
+            }else{
+                key.add(returnArrayList(vtr).get(0));
+                lvl++;
+
+            }
+            if(vtr.startsWith("20")){
+                String mtr =vtr.substring(vtr.indexOf(",")+1);
+                vtr = "";
+                vtr = mtr;
+            }
+            map.put(key.get(lvl),returnArrayList(vtr));
+
+        }
+        return map;
+    }
+    private ArrayList<String> returnArrayList(String vtr){
+        String addString = "";
+
+        ArrayList<String> line = new ArrayList<String>();
+        for(int i = 0; i < vtr.length(); i++){
+            if(vtr.charAt(i) != ','){
+                addString = addString + vtr.charAt(i);
+            }else{
+                line.add(addString);
+                addString = "";
+            }
+
+        }
+        return line;
+    }
+    public String getStudentsAttendance(Map<String,ArrayList<String>> inputMap,String date,String student) {
+        int studentNumber = inputMap.get("name").indexOf(student);
+        try{
+            if(studentNumber == -1){
+                return "Student Not Found";
+            }else{
+                return inputMap.get(date).get(studentNumber);
+            }
+
+        }catch(Exception ignored){
+
+        }
+        return inputMap.get(date).get(studentNumber);
+    }
+}

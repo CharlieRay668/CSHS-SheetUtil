519b52843e120a990818aaae32006b2274423b3d
lauren45983 <lauren.wagner.551@gmail.com>
2020-11-16 11:04:20 -0700

    changed attendence_sheet.py to export as csv

--- attendence_sheet.py
+++ attendence_sheet.py
@@ -5,7 +5,9 @@ import numpy as np
 
 pd.read_csv('fake_data.csv', delimiter = ',')
 
-df = pd.read_csv('fake_data.csv', delimiter = ',') 
+df = pd.read_csv('fake_data.csv', delimiter = ',')
+
+rdf = pd.DataFrame([])
 
 names = (df.columns)
 counter =-1
@@ -14,12 +16,17 @@ for x in names:
     total = 0 # total is the total length of the class 
     attended = 0 # attended is the amount of time the student actually attends class 
     if counter > 0:
-        print(x) # prints out the student's name 
+        
+       # adds row with students name to rdf dataframe 
         for i in range(len(df)): 
             total = total+1
             if df.loc[i, x] > 0:
                 attended = attended + 1
-        print(round((attended/total) * 100))
+       # new_row = {'Student Name': x, 'Percentage Attended': round((attended/total) * 100)}
+        rdf = rdf.append(pd.DataFrame({'Student Name': x, 'Percentage Attended': round((attended/total) * 100)}, index=[0]), ignore_index=True)
+print (rdf.head())
+rdf.to_csv('percentageattended.csv', index=False)
+
     
 
 
